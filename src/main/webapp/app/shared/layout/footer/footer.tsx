59f9333b477509a6d9ec355f88cfebc7e0f39158
Gabriel <tuxomaiworm@hotmail.com>
2022-03-01 19:10:06 -0300

    footer

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -1,16 +1,26 @@
 import './footer.scss';
 
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube, faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 import { Col, Row } from 'reactstrap';
 
 const Footer = () => (
-  <div className="footer page-content">
-    <Row>
-      <Col md="12">
-        <p>Your footer</p>
-      </Col>
-    </Row>
+  <div className="social-container">
+    <h3>Redes Sociais</h3>
+    <a href="https://www.youtube.com/c/jamesqquick" className="youtube social">
+      <FontAwesomeIcon icon={faYoutube} size="2x" />
+    </a>
+    <a href="https://www.facebook.com/learnbuildteach/" className="facebook social">
+      <FontAwesomeIcon icon={faFacebook} size="2x" />
+    </a>
+    <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+      <FontAwesomeIcon icon={faTwitter} size="2x" />
+    </a>
+    <a href="https://www.instagram.com/learnbuildteach" className="instagram social">
+      <FontAwesomeIcon icon={faInstagram} size="2x" />
+    </a>
   </div>
 );
 
